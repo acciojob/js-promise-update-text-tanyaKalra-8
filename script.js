aaef0b7d92160f4a42e7a34e191ed12de7f547e7
tanyaKalra-8 <108450417+tanyaKalra-8@users.noreply.github.com>
2023-05-29 16:04:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function test() {
+	let prom = new Promise((resolve) =>{
+		setTimeout(() => {
+			resolve("Hello, world!")
+		}, 1000)
+	});
+
+	prom.then((data) =>{
+		let text = document.createElement("div")
+		text.innerText = data;
+		document.body.div.append(text);
+	})
+}
